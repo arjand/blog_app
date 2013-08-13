9fc46be2e2eafd826bf95f12e8621645d87d7ac2
Arjan Dehar <adehar@gmail.com>
2013-08-12 19:24:17 -0700

    Updated routes.rb to use 'match' and updated templates accordingly. made styles more sassy

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,15 +6,15 @@ describe "StaticPages" do
 
   describe "Home page" do
     it "should have the content 'Blog App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Blog App')      
     end
     it "should have the right title" do
-    	visit '/static_pages/home'
+    	visit root_path
     	expect(page).to have_title("#{base_title} ")
     end
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title("#{base_title} | Home")
     end
     
@@ -22,33 +22,33 @@ describe "StaticPages" do
 
   describe "Help page" do
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
   	end
   	it "should have the right title" do
-    	visit '/static_pages/help'
+    	visit help_path
     	expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
   	it "should have the content 'About'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_content('About')
   	end
   	it "should have the right title" do
-    	visit '/static_pages/about'
+    	visit about_path
     	expect(page).to have_title("#{base_title} | About")
     end
   end
 
   describe "Contact page" do
   	it "should have the content 'Contact'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		expect(page).to have_content('Contact')
   	end
   	it "should have the right title" do
-    	visit '/static_pages/contact'
+    	visit contact_path
     	expect(page).to have_title("#{base_title} | Contact")
     end
   end
